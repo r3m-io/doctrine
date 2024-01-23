6f6dad77100303e61839041cf3334d2ae034f8cd
remco van der Velde <remco.pc@outlook.com>
2024-01-23 16:18:31 +0100

    init doctrine

---
+++ src/Package/Trait/Main.php
@@ -0,0 +1,50 @@
+<?php
+namespace Package\R3m\Io\Doctrine\Trait;
+
+use R3m\Io\Config;
+
+use R3m\Io\Node\Model\Node;
+
+use Exception;
+
+use R3m\Io\Exception\ObjectException;
+
+trait Main {
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    public function system_config($options=[]): void
+    {
+        $object = $this->object();
+        $posix_id = $object->config(Config::POSIX_ID);
+        if(
+            !in_array(
+                $posix_id,
+                [
+                    0,
+                    33
+                ]
+            )
+        ){
+            throw new Exception('Access denied...');
+        }
+        $node = new Node($object);
+        $config = $node->record('System.Config', $node->role_system());
+        if(
+            $config &&
+            is_array($config) &&
+            array_key_exists('node', $config) &&
+            property_exists($config['node'], 'uuid') &&
+            !property_exists($config['node'], 'server')
+        ){
+            $patch = (object) [
+                'uuid' => $config['node']->uuid,
+                'doctrine' => '*'
+            ];
+            $config = $node->patch('System.Config', $node->role_system(), $patch);
+            ddd($config);
+        }
+    }
+}
--- src/Package/View/Setup/Setup.tpl
+++ src/Package/View/Setup/Setup.tpl
@@ -1,1 +1,7 @@
-### Setup
+{{R3M}}
+{{$register = Package.R3m.Io.Import:Init:register()}}
+{{if(!is.empty($register))}}
+{{Package.R3m.Io.Doctrine:Import:role.system()}}
+{{$options = options()}}
+{{Package.R3m.Io.Server:Main:system.config($options)}}
+{{/if}}
