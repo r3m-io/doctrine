d93b93bd14bd6a5273029d0c2bcd33e1af47a61d
remco van der Velde <remco.pc@outlook.com>
2024-02-26 13:00:08 +0100

    added entity

---
+++ src/Service/Entity.php
@@ -0,0 +1,80 @@
+<?php
+namespace R3m\Io\Doctrine\Service;
+
+use R3m\Io\App;
+use R3m\Io\Module\Controller;
+use R3m\Io\Module\Data;
+
+use Exception;
+
+class Entity extends Main
+{
+
+    /**
+     * @throws Exception
+     */
+    public static function is_granted($class, $role, $options): bool
+    {
+        if(!array_key_exists('function', $options)){
+            throw new Exception('Function is missing in options');
+        }
+        $name = Controller::name($class);
+        $name_permission = str_replace('.', ':', $name);
+        $function_permission = str_replace('_', '.', $options['function']);
+        $role = new Data($role);
+        $is_permission = false;
+        $is_permission_relation = false;
+        $is_permission_parse = false;
+        $permissions = [];
+        $permissions[] = $name_permission . ':' . $function_permission;
+        if(
+            array_key_exists('relation', $options) &&
+            $options['relation'] === true
+        ){
+            $permissions[] = $name_permission . ':' . $function_permission . '.' . 'relation';
+        }
+        if(
+            array_key_exists('parse', $options) &&
+            $options['parse'] === true
+        ){
+            $permissions[] = $name_permission . ':' . $function_permission . '.' . 'parse';
+        }
+        $role_permissions = $role->get('permission');
+        if(is_array($role_permissions)){
+            foreach($role->get('permission') as $permission){
+                $permission = new Data($permission);
+                if($permission->get('name') === $name_permission . ':' .$function_permission){
+                    $is_permission = true;
+                }
+                if(
+                    array_key_exists('relation', $options) &&
+                    $options['relation'] === true
+                ){
+                    if($permission->get('name') === $name_permission . ':' .$function_permission . '.' . 'relation'){
+                        $is_permission_relation = true;
+                    }
+                } else {
+                    $is_permission_relation = true;
+                }
+                if(
+                    array_key_exists('parse', $options) &&
+                    $options['parse'] === true
+                ) {
+                    if($permission->get('name') === $name_permission . ':' . $function_permission . '.' . 'parse'){
+                        $is_permission_parse = true;
+                    }
+                } else {
+                    $is_permission_parse = true;
+                }
+                if(
+                    $is_permission === true &&
+                    $is_permission_parse === true &&
+                    $is_permission_relation === true
+                ){
+                    return true;
+                }
+            }
+        }
+        throw new Exception('Security: permission denied... (' . implode(', ', $permissions) . ')');
+    }
+}
