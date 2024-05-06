a2334e82baa8b247d0606dbe59796782d400ae00
remco van der Velde <remco.pc@outlook.com>
2024-05-06 15:05:02 +0200

    table create using a json schema

--- src/Service/Schema.php
+++ src/Service/Schema.php
@@ -22,6 +22,54 @@ class Schema extends Main
         if(!property_exists($options, 'url')){
             throw new Exception('Option, Url not set...');
         }
+        $read = $object->data_read($options->url);
+        if($read){
+            $has_schema = $read->has('Schema');
+            if($has_schema){
+                $table = $read->get('Schema.table');
+                $entity = $read->get('Schema.entity');
+                $target = $object->config('project.dir.source') .
+                    'Entity' .
+                    $object->config('ds') .
+                    $entity .
+                    $object->config('extension.php')
+                ;
+
+                $use = [];
+                $use[] = 'DateTime';
+                $use[] = '';
+                $use[] = 'Defuse\Crypto\Crypto';
+                $use[] = 'Defuse\Crypto\Exception\BadFormatException';
+                $use[] = 'Defuse\Crypto\Exception\EnvironmentIsBrokenException';
+                $use[] = 'Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException';
+                $use[] = 'Defuse\Crypto\Key';
+                $use[] = '';
+                $use[] = 'Doctrine\ORM\Mapping as ORM';
+                $use[] = 'Doctrine\ORM\Mapping\PrePersist';
+                $use[] = 'Doctrine\ORM\Mapping\PreUpdate';
+                $use[] = 'Doctrine\ORM\Event\PrePersistEventArgs';
+                $use[] = 'Doctrine\ORM\Event\PreUpdateEventArgs';
+                $use[] = '';
+                $use[] = 'R3m\Io\App';
+                $use[] = '';
+                $use[] = 'R3m\Io\Module\Core';
+                $use[] = 'R3m\Io\Module\File';
+                $use[] = '';
+                $use[] = 'Exception';
+                $use[] = '';
+                $use[] = 'R3m\Io\Exception\FileWriteException';
+
+                $data = [];
+                $data[] = '<?php';
+                $data[] = '';
+                $data[] = 'namespace Entity;';
+                foreach($use as $usage){
+                    $data[] = 'use ' . $usage . ';';
+                }
+                ddd($data);
+            }
+        }
+
         ddd($options);
     }
 
