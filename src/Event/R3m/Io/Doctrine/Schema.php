b013efc9f4f4933e844f70f2e3eb4ddb736d3580
remco van der Velde <remco.pc@outlook.com>
2024-05-13 01:53:05 +0200

    add event to create entity classes and sql

--- src/Event/R3m/Io/Doctrine/Schema.php
+++ src/Event/R3m/Io/Doctrine/Schema.php
@@ -21,17 +21,15 @@ class Schema {
     public static function create(App $object, $event, $options=[]): void
     {
         //if exist rename table
-ddd($options);
         if(
             array_key_exists('node', $options) &&
-            property_exists($options, 'node') &&
-            property_exists($options->node, 'environment') &&
+            property_exists($options['node'], 'environment') &&
             (
-                is_array($options->node->environment) ||
-                is_object($options->node->environment)
+                is_array($options['node']->environment) ||
+                is_object($options['node']->environment)
             )
         ) {
-            foreach($options->node->environment as $name => $environment){
+            foreach($options['node']->environment as $name => $environment){
                 $is_rename = false;
                 $table = null;
                 if(
