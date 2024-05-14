421eedff7d60dff0962b973dd714fdbfd3ced13c
remco van der Velde <remco.pc@outlook.com>
2024-05-14 19:28:35 +0200

    add event to create entity classes and sql

--- src/Service/Table.php
+++ src/Service/Table.php
@@ -119,6 +119,36 @@ class Table extends Main
     /**
      * @throws Exception
      */
+    public static function columns($object, $name, $environment=null, $options=null): bool
+    {
+        $options = Core::object($options);
+        if ($environment === null) {
+            $environment = $object->config('environment');
+        } else {
+            $environment = str_replace('.', '-', $environment);
+        }
+        $name = str_replace('.', '-', $name);
+        if (!property_exists($options, 'table')) {
+            throw new Exception('table not set in options');
+        }
+        $connection = Database::connection($object, $name, $environment);
+        if (!$connection) {
+            Database::instance($object, $name, $environment);
+            $connection = Database::connection($object, $name, $environment);
+        }
+        $tables = Table::all($object, $name, $environment);
+        $sanitized_table = preg_replace('/[^a-zA-Z0-9_]/', '', $options->table);
+        if (in_array($options->table, $tables, true)) {
+            $sql = "PRAGMA table_info($sanitized_table)";
+            $stmt = $connection->executeQuery($sql);
+            return $stmt->fetchAllAssociative();
+        }
+        return [];
+    }
+
+    /**
+     * @throws Exception
+     */
     public static function delete($object, $name, $environment=null, $options=null): bool
     {
         $options = Core::object($options);
